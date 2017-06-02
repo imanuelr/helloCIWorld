a3f9a50881dba93ff556c44f5435ca026e2c76ad
Imanuel Rennen <imanuel.rennen@telenetgroup.be>
2017-06-02 15:01:01 +0200

    inspect build script

--- concourseCI/tasks/build-artifact.sh
+++ concourseCI/tasks/build-artifact.sh
@@ -3,10 +3,18 @@
 set -x
 
 export TERM=${TERM:-dumb}
+
+ls -la
+
 cd app-repository
 chmod +x gradlew
 
 ./gradlew --no-daemon build
+cd build/
+ls -la
+
+cd ../..
 
 git clone resource-share updated-gist
+
 ls -la
