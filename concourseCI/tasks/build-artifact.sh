7711e5084ad2f8570dc797a4737de75655e3dc3b
Imanuel Rennen <imanuel.rennen@telenetgroup.be>
2017-06-02 15:18:06 +0200

    attempt to commit artifacts to gist

--- concourseCI/tasks/build-artifact.sh
+++ concourseCI/tasks/build-artifact.sh
@@ -11,5 +11,9 @@ rm -rf updated-gist/build
 cp -a app-repository/build updated-gist
 
 cd updated-gist
+
+git config --global user.email "nobody@concourse.ci"
+git config --global user.name "Concourse"
+
 git add .
 git commit -m "Push new artifacts"
