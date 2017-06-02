0636bbe269ba75e733e21a156e4570a214d0dbe1
Imanuel Rennen <imanuel.rennen@telenetgroup.be>
2017-06-02 14:51:15 +0200

    sharing resources

--- concourseCI/tasks/build-artifact.sh
+++ concourseCI/tasks/build-artifact.sh
@@ -5,4 +5,8 @@ set -x
 export TERM=${TERM:-dumb}
 cd app-repository
 chmod +x gradlew
-./gradlew --no-daemon compileJava compileTestJava
+
+./gradlew --no-daemon build
+
+git clone resource-share updated-gist
+ls -la
